560abc229648713aa47c0702b2ab8f1377cc482f
Russell Carlin <recarlin@fullsail.com>
2012-02-09 03:32:30 -0500

    Basically finished
    I think I have it pretty much finished. I may do some tweaking tomorrow.

--- JS/main.js
+++ JS/main.js
@@ -2,49 +2,60 @@
 // MIU 1202
 // Project 1
 $(document).ready(function(){
-    $(':jqmData(url^=index.html)').live('pagebeforecreate', 
-  function(event) {
-    $(this).filter(':jqmData(url*=ui-page)').find(':jqmData(role=header)')
-      .prepend('<a href="#" data-rel="back" data-icon="back">Back</a>')
-  });
-    $("#clear").bind("click", function(){
-        if(localStorage.length != 0) {
-            localStorage.clear();
-            alert("Your guides have been deleted.")
-            window.location.reload();
-        } else {
-            alert("You have no guides saved.")
-        };
-    }); 
-    $("#goodBrowse").bind("pageshow", function(){     
-        if (localStorage.length === 0){
-            for(var n in filler) {
-                var id = Math.floor(Math.random()*100000000);
-                localStorage.setItem(id, JSON.stringify(filler[n]));
-            };
-        };
-        var makeList = document.getElementById("guides")
-        for(i=0, l=localStorage.length; i<l; i++) {
-            var makeli = document.createElement("li"),
-                makeA = document.createElement("a");
-            makeA.setAttribute("href", "#details")
-            makeList.appendChild(makeA);
-            makeA.appendChild(makeli);
-            var key = localStorage.key(i),
-                value = localStorage.getItem(key),
-                guide = JSON.parse(value),
-                titleText = guide.game[1] + " - " + guide.title[1]
-                makeSubList = document.createElement("ul");
-            makeli.innerHTML = titleText;
-            makeli.appendChild(makeSubList);
-            makeSubList.style.listStyle = "none";
-            for(var g in guide) {
-                var makeSubli = document.createElement("li");
-                makeSubList.appendChild(makeSubli);
-                var guideSubText = guide[g][0] + ": " + guide[g][1];
-                    makeSubli.innerHTML = guideSubText;
-            };
-        };
-        $("#guides").listview("refresh");
+    $("#clickLoL").click(function(){
+     window.location=$(this).find("a").attr("href");
+     return false;
     });
+    $("#clickWoW").click(function(){
+     window.location=$(this).find("a").attr("href");
+     return false;
+    });
+    $("#clickSky").click(function(){
+     window.location=$(this).find("a").attr("href");
+     return false;
+    });
+    $("#clickTQ").click(function(){
+     window.location=$(this).find("a").attr("href");
+     return false;
+    });
+    //$("#clear").bind("click", function(){
+    //    if(localStorage.length != 0) {
+    //        localStorage.clear();
+    //        alert("Your guides have been deleted.")
+    //        window.location.reload();
+    //    } else {
+    //        alert("You have no guides saved.")
+    //    };
+    //}); 
+    //$("#goodBrowse").bind("pageshow", function(){     
+    //    if (localStorage.length === 0){
+    //        for(var n in filler) {
+    //            var id = Math.floor(Math.random()*100000000);
+    //            localStorage.setItem(id, JSON.stringify(filler[n]));
+    //        };
+    //    };
+    //    var makeList = document.getElementById("guides")
+    //    for(i=0, l=localStorage.length; i<l; i++) {
+    //        var makeli = document.createElement("li"),
+    //            makeA = document.createElement("a");
+    //        makeA.setAttribute("href", "#details")
+    //        makeList.appendChild(makeA);
+    //        makeA.appendChild(makeli);
+    //        var key = localStorage.key(i),
+    //            value = localStorage.getItem(key),
+    //            guide = JSON.parse(value),
+    //            titleText = guide.game[1] + " - " + guide.title[1]
+    //            makeSubList = document.createElement("ul");
+    //        makeli.innerHTML = titleText;
+    //        makeli.appendChild(makeSubList);
+    //        makeSubList.style.listStyle = "none";
+    //        for(var g in guide) {
+    //            var makeSubli = document.createElement("li");
+    //            makeSubList.appendChild(makeSubli);
+    //            var guideSubText = guide[g][0] + ": " + guide[g][1];
+    //                makeSubli.innerHTML = guideSubText;
+    //        };
+    //    };
+    //    $("#guides").listview("refresh");
+    //});
 });
