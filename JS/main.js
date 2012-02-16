72475d1ab4b225ecec9d222f031954ce58b4a38b
Russell Carlin <recarlin@fullsail.com>
2012-02-15 22:47:34 -0500

    Update
    Got my dynamic selector working and edited the look of things. Got part
    of my validation working, just need to style it.

--- JS/main.js
+++ JS/main.js
@@ -5,35 +5,44 @@ var types = {
         LoL:["Bruiser", "AP Carry", "AD Carry", "Support", "Jungle"],
         WoW:["Death Knight", "Druid", "Hunter", "Mage", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"],
         Skyrim:["Caster", "Melee", "Ranger", "Stealth", "Tank"],
+        MC: ["Redstone", "Builds", "Servers", "Textures", "Adventure"],
         TQ: ["Earth", "Defense", "Dream", "Hunting", "Nature", "Rogue", "Spirit", "Storm", "Warfare"]
     };
 $(document).ready(function(){
+    
     $("#clickLoL").click(function(){
      window.location=$(this).find("a").attr("href");
      return false;
     });
+    
     $("#clickWoW").click(function(){
      window.location=$(this).find("a").attr("href");
      return false;
     });
+    
     $("#clickSky").click(function(){
      window.location=$(this).find("a").attr("href");
      return false;
     });
+    
     $("#clickTQ").click(function(){
      window.location=$(this).find("a").attr("href");
      return false;
     });
+   
     $("#clickMC").click(function(){
      window.location=$(this).find("a").attr("href");
      return false;
     });
-    $("#game").change(function(){
-        $.each(types, function(key, value) {   
-        	$('#type')
-        	.append($('<option>', { value : key })
-        	.text(value)); 
-});
-		type.selectmenu("refresh");
+   
+    $('select#game').change(function() {
+    	var x= $('#game').val()
+    	$('#type').find("option").remove()
+        for(index in types[x]) {
+        	$('#type').append('<option value="' + types[x][index] + '">' + types[x][index] + '</option>')
+        };
+    $('#type').selectmenu('refresh', true);
     });
-});
+    
+    $('#guideForm').validate()
+})
