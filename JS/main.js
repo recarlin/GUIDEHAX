2aaf69f2591e641301939eea8942f1fa5b6bb0d9
Russell Carlin <recarlin@fullsail.com>
2012-02-15 19:47:00 -0500

    Another update
    styled the form a little more an about to do my bad version.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Russell Carlin
 // MIU 1202
-// Project 1
+// Project 3
 var types = {
         LoL:["Bruiser", "AP Carry", "AD Carry", "Support", "Jungle"],
         WoW:["Death Knight", "Druid", "Hunter", "Mage", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"],
@@ -29,51 +29,11 @@ $(document).ready(function(){
      return false;
     });
     $("#game").change(function(){
-        function popType(x) {
-            var gt = $("#type");
-            gt.options.length = 0
-            for(index in types[x]) {
-                gt.options[gt.options.length] = new Option(types[x][index], types[x][index]);
-            };
-        };
-    };
-    //$("#clear").bind("click", function(){
-    //    if(localStorage.length != 0) {
-    //        localStorage.clear();
-    //        alert("Your guides have been deleted.")
-    //        window.location.reload();
-    //    } else {
-    //        alert("You have no guides saved.")
-    //    };
-    //}); 
-    //$("#goodBrowse").bind("pageshow", function(){     
-    //    if (localStorage.length === 0){
-    //        for(var n in filler) {
-    //            var id = Math.floor(Math.random()*100000000);
-    //            localStorage.setItem(id, JSON.stringify(filler[n]));
-    //        };
-    //    };
-    //    var makeList = document.getElementById("guides")
-    //    for(i=0, l=localStorage.length; i<l; i++) {
-    //        var makeli = document.createElement("li"),
-    //            makeA = document.createElement("a");
-    //        makeA.setAttribute("href", "#details")
-    //        makeList.appendChild(makeA);
-    //        makeA.appendChild(makeli);
-    //        var key = localStorage.key(i),
-    //            value = localStorage.getItem(key),
-    //            guide = JSON.parse(value),
-    //            titleText = guide.game[1] + " - " + guide.title[1]
-    //            makeSubList = document.createElement("ul");
-    //        makeli.innerHTML = titleText;
-    //        makeli.appendChild(makeSubList);
-    //        makeSubList.style.listStyle = "none";
-    //        for(var g in guide) {
-    //            var makeSubli = document.createElement("li");
-    //            makeSubList.appendChild(makeSubli);
-    //            var guideSubText = guide[g][0] + ": " + guide[g][1];
-    //                makeSubli.innerHTML = guideSubText;
-    //        };
-    //    };
-    //    $("#guides").listview("refresh");
-    //});});
+        $.each(types, function(key, value) {   
+        	$('#type')
+        	.append($('<option>', { value : key })
+        	.text(value)); 
+});
+		type.selectmenu("refresh");
+    });
+});
